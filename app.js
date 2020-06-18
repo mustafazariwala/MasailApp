d936f6052d58f45330240f9db8dcb26e38d86c22
mustafazariwala <mustafazariwala5253@gmail.com>
2020-06-18 20:29:16 +0500

    make it better

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const compression = require('compression')
+const bodyParser = require("body-parser");
+const path = require('path')
+
+
+const app = express();
+// app.use(cors())
+app.use(compression())
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use("/", express.static(path.join(__dirname, "angular")))
+
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "*"
+  );
+  next();
+});
+
+app.use((req,res,next)=> {
+  res.sendFile(path.join(__dirname ,"angular", "index.html"))
+})
+
+
+module.exports = app;
